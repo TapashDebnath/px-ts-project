df91ce26a3867dcb7960b11d2c11392c4939fff0
TapashDebnath <52230105+TapashDebnath@users.noreply.github.com>
2020-09-12 21:57:19 +0530

    Updates

---
+++ precache-manifest.1510b311f20ee4a1bf94a90e7372c92d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfbe0b7b164a6c4dbbd0701a6001ca5",
+    "url": "/px-ts/index.html"
+  },
+  {
+    "revision": "d709d8513a28cfd68156",
+    "url": "/px-ts/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4fa23cc10df29d753f15",
+    "url": "/px-ts/static/css/main.4d592bd8.chunk.css"
+  },
+  {
+    "revision": "d709d8513a28cfd68156",
+    "url": "/px-ts/static/js/2.67811176.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/px-ts/static/js/2.67811176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa23cc10df29d753f15",
+    "url": "/px-ts/static/js/main.f51fc699.chunk.js"
+  },
+  {
+    "revision": "7259f004961d89591c42",
+    "url": "/px-ts/static/js/runtime-main.68f99371.js"
+  }
+]);
